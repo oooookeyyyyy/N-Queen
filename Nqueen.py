64e2aa098742a92ef05df12126b45ee0408c828c
oooookeyyyyy <73366852+oooookeyyyyy@users.noreply.github.com>
2023-01-14 04:18:02 +0330

    Update Nqueen.py

--- Nqueen.py
+++ Nqueen.py
@@ -1,46 +1,37 @@
 from copy import deepcopy
 
 def add(l, c) :
-        ln = l
-        cl = c
         for i in range(table) :
-            board[ln][i] = 'O'
-            board[i][cl] = 'O'
-        board[ln][cl] = 'X'
+            board[l][i] = board[i][c] = 'O'
+        board[l][c] = 'X'
+        ln, cl = l, c
         while ln > 0 and cl > 0 :
             board[ln-1][cl-1] = 'O'
             ln -= 1
             cl -= 1
-        ln = l
-        cl = c
+        ln, cl = l, c
         while ln < table - 1 and cl < table - 1 :
             board[ln+1][cl+1] = 'O'
             ln += 1
             cl += 1
-        ln = l
-        cl = c
+        ln, cl = l, c
         while ln > 0 and cl < table - 1 :
             board[ln-1][cl+1] = 'O'
             ln -= 1
             cl += 1
-        ln = l
-        cl = c
+        ln, cl = l, c
         while ln < table - 1 and cl > 0 :
             board[ln+1][cl-1] = 'O'
             ln += 1
             cl -= 1
 
 table = int(input('input Side length >>'))
-board = []
-stack = []
 spots = []
 result = []
 solution = 0
-l = 0
-c = 0
-for i in range(table) :
-    board.append(['0' for i in range(table)])
-stack.append(deepcopy(board))
+l, c = 0, 0
+board = [['0' for _ in range(table)] for _ in range(table)]
+stack = [deepcopy(board)]
 while l < table :
     while c < table :
         if '0' not in stack[-1][l][c:] :
@@ -49,14 +40,14 @@ while l < table :
                 board = deepcopy(stack[-1])
                 l -= 1
                 c = spots.pop()+1
-                if c == table : 
+                if c == table:
                     stack.pop()
                     board = deepcopy(stack[-1])
                     l -= 1
                     c = spots.pop()+1
                 break
             except :
-                print(str(solution) + ' optimal state : ')
+                print(f'{solution} optimal state : ')
                 input('press enter to show boards')
                 for r in result :
                     print('-----'*table)
